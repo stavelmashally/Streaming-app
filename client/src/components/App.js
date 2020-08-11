5734725c20f11686d20fc65b5d46014a02d36d39
Stav Elmashally <stavelmashally6@gmail.com>
2020-08-11 12:08:29 +0300

    StreamShow screen

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import history from '../history';
-import Header from './Header'
-import StreamList from './streams/StreamList'
+import Header from './Header';
+import StreamList from './streams/StreamList';
 import StreamEdit from './streams/StreamEdit';
 import StreamCreate from './streams/StreamCreate';
 import StreamShow from './streams/StreamShow';
@@ -14,11 +14,13 @@ const App = props => {
       <Router history={history}>
         <div>
           <Header />
-          <Route path="/" exact component={StreamList} />
-          <Route path="/streams/new" exact component={StreamCreate} />
-          <Route path="/streams/delete/:id" exact component={StreamDelete} />
-          <Route path="/streams/show" exact component={StreamShow} />
-          <Route path="/streams/edit/:id" exact component={StreamEdit} />
+          <Switch>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/delete/:id" exact component={StreamDelete} />
+            <Route path="/streams/:id" exact component={StreamShow} />
+            <Route path="/streams/edit/:id" exact component={StreamEdit} />
+          </Switch>
         </div>
       </Router>
     </div>
